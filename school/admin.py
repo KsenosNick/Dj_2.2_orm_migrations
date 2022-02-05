7a4419a73abd215e43d13661c1350f98097bdb87
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-05 19:00:09 +0300

    Настроена админка

--- school/admin.py
+++ school/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, StudentTeacher
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'subject']
+
+
+class TeacherInline(admin.TabularInline):
+    model = StudentTeacher
+    extra = 0
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'group']
+    inlines = [TeacherInline]
